f55559f844792d549654da6abcee140d20d4aea3
Fisman Maxim <mfisman05@gmail.com>
2023-11-12 18:51:40 +0300

    Added custom exceptions

---
+++ src/main/java/CustomExceptions/EntityIdNotFoundException.java
@@ -0,0 +1,16 @@
+package CustomExceptions;
+
+public class EntityIdNotFoundException extends Exception {
+
+    public EntityIdNotFoundException() {
+        super();
+    }
+
+    public EntityIdNotFoundException(String message) {
+        super(message);
+    }
+
+    public EntityIdNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
