134b40c8e677023f545b8cf11d012eadd273322d
IgorShishkin12 <qwertyshishkin@yandex.ru>
2023-12-19 18:28:30 +0300

    added test database to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,25 +6,39 @@ import EntitiesServices.UserService;
 import InMemoryRepos.InMemoryCompanyRepository;
 import InMemoryRepos.InMemoryUserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import spark.Service;
+ import database.Database;
+ import spark.Service;
 
-import java.util.List;
+ import java.sql.DriverManager;
+ import java.sql.SQLException;
+ import java.util.List;
 public class Main {
 
     private static final ObjectMapper mapper = new ObjectMapper();
 
     public static void main(String[] args) {
-        Service service = Service.ignite();
-        CompanyService companyService = new CompanyService(new InMemoryCompanyRepository());
+        try {
+            Service service = Service.ignite();
+            Database database = new Database(DriverManager.getConnection("jdbc" +
+                            ":postgresql" +
+                            "://cornelius.db.elephantsql.com:5432/hmtdjque",
+                    "hmtdjque",
+                    "mW9O7Imtz3eqjtvVolLGZ4gWlC9VuKMh"));
+            CompanyService companyService =
+                    new CompanyService(database.company);
 
-        UserService userService = new UserService(new InMemoryUserRepository());
-        ControllersManager manager = new ControllersManager(List.of(
-                new CompanyController(service, companyService, mapper),
-                new UserController(service, userService, companyService, mapper)
-        ));
+            UserService userService = new UserService(database.user);
+            ControllersManager manager = new ControllersManager(List.of(
+                    new CompanyController(service, companyService, mapper),
+                    new UserController(service, userService, companyService, mapper)
+            ));
 
-        manager.start();
-        service.awaitInitialization();
-        service.awaitStop();
+            manager.start();
+            service.awaitInitialization();
+            service.awaitStop();
+        }
+        catch (SQLException e){
+
+        }
     }
 }
